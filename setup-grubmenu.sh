f118fc18e9c86a1c3a9bc793c67a4504d2baffab
dido11 <v_m_1732@orange.fr>
2022-06-21 13:08:20 +0200

    Added missing / in sed commands

--- setup-grubmenu.sh
+++ setup-grubmenu.sh
@@ -7,7 +7,7 @@ fi
 
 set -e
 
-sed -i s"/^GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=-1" /etc/default/grub
-sed -i s"/^GRUB_TIMEOUT_STYLE=.*$/GRUB_TIMEOUT_STYLE=menu" /etc/default/grub
+sed -i s"/^GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=-1/" /etc/default/grub
+sed -i s"/^GRUB_TIMEOUT_STYLE=.*$/GRUB_TIMEOUT_STYLE=menu/" /etc/default/grub
 
 update-grub
